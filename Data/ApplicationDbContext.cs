495ea1d0dcb5625e3f01c1b7b0a3c6c493020d61
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-07-07 13:43:14 +0300

    Configure ApplicationDbContext with WorkOrder, Document, and Image models

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ManagementApp.Models;
+
+
+namespace ManagementApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<WorkOrder>()
+                .HasMany(t => t.Images)
+                .WithOne(i => i.WorkOrder)
+                .HasForeignKey(i => i.WorkOrderId);
+
+            builder.Entity<WorkOrder>()
+                .HasMany(t => t.Documents)
+                .WithOne(d => d.WorkOrder)
+                .HasForeignKey(d => d.WorkOrderId);
+        }
+    }
+}
