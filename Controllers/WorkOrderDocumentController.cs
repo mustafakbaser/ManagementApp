3436f498f8858dc2e366b07c0fac41455a8e7fc9
Mustafa Kürşad BAŞER <mkursatbaser@gmail.com>
2024-07-17 23:41:23 +0300

    Add Document and Image controllers

---
+++ Controllers/WorkOrderDocumentController.cs
@@ -0,0 +1,67 @@
+﻿using ManagementApp.Models;
+using ManagementApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkOrderDocumentController : ControllerBase
+    {
+        private readonly IWorkOrderDocumentService _documentService;
+
+        public WorkOrderDocumentController(IWorkOrderDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDocuments()
+        {
+            var documents = await _documentService.GetAllDocumentsAsync();
+            return Ok(documents);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDocumentById(int id)
+        {
+            var document = await _documentService.GetDocumentByIdAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            return Ok(document);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDocument([FromBody] Document document)
+        {
+            var createdDocument = await _documentService.AddDocumentAsync(document);
+            return CreatedAtAction(nameof(GetDocumentById), new { id = createdDocument.Id }, createdDocument);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDocument(int id, [FromBody] Document document)
+        {
+            if (id != document.Id)
+            {
+                return BadRequest();
+            }
+
+            var updatedDocument = await _documentService.UpdateDocumentAsync(document);
+            return Ok(updatedDocument);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocument(int id)
+        {
+            var result = await _documentService.DeleteDocumentAsync(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ Controllers/WorkOrderImageController.cs
@@ -0,0 +1,67 @@
+﻿using ManagementApp.Models;
+using ManagementApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkOrderImageController : ControllerBase
+    {
+        private readonly IWorkOrderImageService _imageService;
+
+        public WorkOrderImageController(IWorkOrderImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllImages()
+        {
+            var images = await _imageService.GetAllImagesAsync();
+            return Ok(images);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetImageById(int id)
+        {
+            var image = await _imageService.GetImageByIdAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            return Ok(image);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddImage([FromBody] Image image)
+        {
+            var createdImage = await _imageService.AddImageAsync(image);
+            return CreatedAtAction(nameof(GetImageById), new { id = createdImage.Id }, createdImage);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateImage(int id, [FromBody] Image image)
+        {
+            if (id != image.Id)
+            {
+                return BadRequest();
+            }
+
+            var updatedImage = await _imageService.UpdateImageAsync(image);
+            return Ok(updatedImage);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var result = await _imageService.DeleteImageAsync(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
